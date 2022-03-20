8bbe03a753faef1854d5e8cd7cd43afdc399da87
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-20 16:59:29 +0100

    Forwarding port for debugging. Adding uvicorn inside main.py.

--- src/main.py
+++ src/main.py
@@ -11,9 +11,16 @@ def plan_route(params: RouteRequest):
     return RouteResponse(
         distance_km=168.43,
         duration_minutes=95,
-        route=["Ostrava", "Bilovec", "Hranice", "Olomouc", "Prostejov", "Vyskov", "Brno"],
+        route=["Ostrava", "Bilovec", "Hranice",
+               "Olomouc", "Prostejov", "Vyskov", "Brno"],
     )
+
 
 @app.get("/test")
 def get_test_response():
     return TestResponse(test_response="Everyting is ok")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
