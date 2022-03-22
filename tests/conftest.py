dec29a8bfca5a9f7d9c3c305b24f8ab28aaaeeca
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-22 17:53:58 +0100

    Added sqlite_database fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from tempfile import NamedTemporaryFile
 
 from main import app
+from db import db_manager
 
 
 @pytest.fixture
@@ -14,3 +15,24 @@ async def client():
 async def temp_file():
     with NamedTemporaryFile() as temporary_file:
         yield temporary_file.name
+
+
+class DbManagerGuard:
+    def __init__(self, db_manager, db_url):
+        self._db_manager = db_manager
+        self._db_manager.reset(db_url=db_url)
+
+    async def __aenter__(self):
+        await self._db_manager.up()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self._db_manager.down()
+        self._db_manager.reset()
+
+
+@pytest.fixture
+async def sqlite_database(temp_file):
+    db_url = 'sqlite+aiosqlite:///' + temp_file
+    async with DbManagerGuard(db_manager=db_manager, db_url=db_url):
+        yield db_manager.get_database()
--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -4,15 +4,9 @@ from db import db_manager
 from schema import cities
 
 
-async def test_reset(temp_file):
-    db_url = 'sqlite+aiosqlite:///' + temp_file
-    db_manager.reset(db_url=db_url)
-    await db_manager.up()
-
-    temporary_db = db_manager.get_database()
-
+async def test_reset(sqlite_database):
     tables_query = "SELECT * FROM sqlite_master where type='table';"
-    tables = await temporary_db.fetch_all(query=tables_query)
+    tables = await sqlite_database.fetch_all(query=tables_query)
 
     assert len(tables) != 0
 
@@ -21,8 +15,5 @@ async def test_reset(temp_file):
                 "lattitude": 51.509865,
                 "longitude": -0.118092})
 
-    last_record_id = await temporary_db.execute(query=city_query)
-
+    last_record_id = await sqlite_database.execute(query=city_query)
     assert last_record_id == 1
-
-    await db_manager.down()
