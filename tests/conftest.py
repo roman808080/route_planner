4e555c186a7ef28f6e4e181aa8103cea8873b9db
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-22 17:22:18 +0100

    Adding temp_file fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 from fastapi.testclient import TestClient
+from tempfile import NamedTemporaryFile
+
 from main import app
 
 
 @pytest.fixture
 async def client():
     return TestClient(app)
+
+
+@pytest.fixture
+async def temp_file():
+    with NamedTemporaryFile() as temporary_file:
+        yield temporary_file.name
--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -1,30 +1,28 @@
 import pytest
-from tempfile import NamedTemporaryFile
 
-from db import db_manager, DbManager
+from db import db_manager
 from schema import cities
 
 
-async def test_reset():
-    with NamedTemporaryFile() as temporary_db_file:
-        db_url = 'sqlite+aiosqlite:///' + temporary_db_file.name
-        db_manager.reset(db_url=db_url)
-        await db_manager.up()
+async def test_reset(temp_file):
+    db_url = 'sqlite+aiosqlite:///' + temp_file
+    db_manager.reset(db_url=db_url)
+    await db_manager.up()
 
-        temporary_db = db_manager.get_database()
+    temporary_db = db_manager.get_database()
 
-        tables_query = "SELECT * FROM sqlite_master where type='table';"
-        tables = await temporary_db.fetch_all(query=tables_query)
+    tables_query = "SELECT * FROM sqlite_master where type='table';"
+    tables = await temporary_db.fetch_all(query=tables_query)
 
-        assert len(tables) != 0
+    assert len(tables) != 0
 
-        city_query = cities.insert(
-            values={"name": 'London',
-                    "lattitude": 51.509865,
-                    "longitude": -0.118092})
+    city_query = cities.insert(
+        values={"name": 'London',
+                "lattitude": 51.509865,
+                "longitude": -0.118092})
 
-        last_record_id = await temporary_db.execute(query=city_query)
+    last_record_id = await temporary_db.execute(query=city_query)
 
-        assert last_record_id == 1
+    assert last_record_id == 1
 
-        await temporary_db.disconnect()
+    await temporary_db.disconnect()
