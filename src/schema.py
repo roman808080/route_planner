d15b16d1332d19f6d506f4130d9107e668a8b524
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-23 11:08:09 +0100

    Added roads table.

--- src/schema.py
+++ src/schema.py
@@ -12,3 +12,13 @@ cities: schema.Table = sqlalchemy.Table(
     sqlalchemy.Column("lattitude", sqlalchemy.Float),
     sqlalchemy.Column("longitude", sqlalchemy.Float),
 )
+
+roads: schema.Table = sqlalchemy.Table(
+    "roads",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("first_city_id", sqlalchemy.Integer),
+    sqlalchemy.Column("second_city_id", sqlalchemy.Integer),
+    sqlalchemy.Column("distance_km", sqlalchemy.Float),
+    sqlalchemy.Column("duration_minutes", sqlalchemy.Float),
+)
