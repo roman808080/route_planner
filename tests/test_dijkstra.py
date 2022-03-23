ae4b600a787dfff720a03296392cf2fd6e56d1c0
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-23 17:10:06 +0100

    Adding data to db.

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,4 +1,5 @@
 from dijkstra import Graph, dijkstra_algorithm
+from models import City, CityResponse, Road, RoadResponse
 
 
 def get_result(previous_nodes, shortest_path, start_node, target_node):
@@ -15,9 +16,64 @@ def get_result(previous_nodes, shortest_path, start_node, target_node):
     return " -> ".join(reversed(path))
 
 
-def test_dijkstra_algorithm():
+def add_city_to_db(client, name):
+    response = client.post("/city", json=City(name=name,
+                                              lattitude=0, longitude=0).dict())
+    city_response = CityResponse(**response.json())
+    assert city_response.status == 'success'
+
+
+def add_road_to_db(client, first_city, second_city, distance):
+    road = Road(first_city_name=first_city, second_city_name=second_city,
+                distance_km=distance, duration_minutes=1)
+    response = client.post("/road", json=road.dict())
+
+    road_response = RoadResponse(**response.json())
+    assert road_response.status == 'success'
+
+
+def test_dijkstra_algorithm(client, sqlite_database):
     nodes = ["Reykjavik", "Oslo", "Moscow", "London",
              "Rome", "Berlin", "Belgrade", "Athens"]
+
+    for node in nodes:
+        add_city_to_db(client=client, name=node)
+
+    add_road_to_db(client=client,
+                   first_city="Reykjavik", second_city="Oslo",
+                   distance=5)
+
+    add_road_to_db(client=client,
+                   first_city="Reykjavik", second_city="London",
+                   distance=4)
+
+    add_road_to_db(client=client,
+                   first_city="Oslo", second_city="Berlin",
+                   distance=1)
+
+    add_road_to_db(client=client,
+                   first_city="Oslo", second_city="Moscow",
+                   distance=3)
+
+    add_road_to_db(client=client,
+                   first_city="Moscow", second_city="Belgrade",
+                   distance=5)
+
+    add_road_to_db(client=client,
+                   first_city="Moscow", second_city="Athens",
+                   distance=4)
+
+    add_road_to_db(client=client,
+                   first_city="Athens", second_city="Belgrade",
+                   distance=1)
+
+    add_road_to_db(client=client,
+                   first_city="Rome", second_city="Berlin",
+                   distance=2)
+
+    add_road_to_db(client=client,
+                   first_city="Rome", second_city="Athens",
+                   distance=2)
 
     init_graph = {}
     for node in nodes:
@@ -34,7 +90,9 @@ def test_dijkstra_algorithm():
     init_graph["Rome"]["Athens"] = 2
 
     graph = Graph(nodes, init_graph)
-    previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node="Reykjavik")
+    previous_nodes, shortest_path = dijkstra_algorithm(
+        graph=graph, start_node="Reykjavik")
 
-    result = get_result(previous_nodes, shortest_path, start_node="Reykjavik", target_node="Belgrade")
+    result = get_result(previous_nodes, shortest_path,
+                        start_node="Reykjavik", target_node="Belgrade")
     assert "Reykjavik -> Oslo -> Berlin -> Rome -> Athens -> Belgrade" == result
