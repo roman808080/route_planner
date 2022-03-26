9893425bd5936ef4c6b775e1ca1e12b6cfb82136
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-26 20:10:47 +0100

    Fixed tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from models import City, CityResponse, Road, RoadResponse
 
 
 def add_city_to_db(client, name):
-    response = client.post("/city", json=City(name=name,
+    response = client.post("/admin/city", json=City(name=name,
                                               lattitude=0, longitude=0).dict())
     city_response = CityResponse(**response.json())
     assert city_response.status == 'success'
@@ -11,7 +11,7 @@ def add_city_to_db(client, name):
 def add_road_to_db(client, first_city, second_city, distance, duration):
     road = Road(first_city_name=first_city, second_city_name=second_city,
                 distance_km=distance, duration_minutes=duration)
-    response = client.post("/road", json=road.dict())
+    response = client.post("/admin/road", json=road.dict())
 
     road_response = RoadResponse(**response.json())
     assert road_response.status == 'success'
